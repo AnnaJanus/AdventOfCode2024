57e0839c011709eed5e97e59dbe17dec0c1db3da
AnnaJanus <anusiula.789@gmail.com>
2026-03-25 00:11:31 +0100

    Day 1 finished

---
+++ src/test/java/Day1.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Day1 {
+    public static void main(String[] args) {
+        System.out.println(sumDistance("src/test/java/Day1Data.txt"));
+    }
+
+    public static int sumDistance(String filePath) {
+        List<String> lines = new ArrayList<>();
+        List<Integer> leftColumn = new ArrayList<>();
+        List<Integer> rightColumn = new ArrayList<>();
+
+        try {
+            lines = Files.readAllLines(Path.of(filePath));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (String line : lines) {
+            leftColumn.add(Integer.valueOf(line.split(" +")[0]));
+            rightColumn.add(Integer.valueOf(line.split(" +")[1]));
+        }
+
+        Collections.sort(leftColumn);
+        Collections.sort(rightColumn);
+
+        int sum = 0;
+        for (int i = 0; i < leftColumn.size(); i++) {
+            sum += Math.abs(leftColumn.get(i) - rightColumn.get(i));
+        }
+        return sum;
+    }
+}
---
+++ src/test/java/Day1b.java
@@ -0,0 +1,48 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+
+public class Day1b {
+    private static final List<Integer> leftColumn = new ArrayList<>();
+    private static final List<Integer> rightColumn = new ArrayList<>();
+
+    public static void main(String[] args) {
+        loadAndSortLocations("src/test/java/Day1Data.txt");
+        System.out.println(countSimilarity());
+    }
+
+    public static void loadAndSortLocations(String filePath) {
+        List<String> lines = new ArrayList<>();
+        try {
+            lines = Files.readAllLines(Path.of(filePath));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (String line : lines) {
+            leftColumn.add(Integer.valueOf(line.split(" +")[0]));
+            rightColumn.add(Integer.valueOf(line.split(" +")[1]));
+        }
+
+        Collections.sort(leftColumn);
+        Collections.sort(rightColumn);
+    }
+
+    private static int countSimilarity() {
+        Set<Integer> leftColumnWithoutDuplicates = new HashSet<>(leftColumn);
+        int similarity = 0;
+        for (int leftNumber : leftColumnWithoutDuplicates) {
+            int counter = 0;
+            for (int rightNumber : rightColumn) {
+                if (leftNumber == rightNumber) {
+                    counter++;
+                } else if (leftNumber < rightNumber) {
+                    break;
+                }
+            }
+            similarity += counter * leftNumber;
+        }
+        return similarity;
+    }
+}
